306daab51b50dfa4dd63cb7edcbb7dfb6f33b295
seb <sebastien.alliot@eemi.com>
2021-01-24 17:21:29 +0100

    Ajout graph top 5 client nb credits

--- web_app_flask/app/models.py
+++ web_app_flask/app/models.py
@@ -7,3 +7,23 @@ class HomeCreditColumnsDescription(db.Model):
     Row = db.Column(db.String(120))
     Description = db.Column(db.String(120))
     Special = db.Column(db.String(120))
+
+
+class Bureau(db.Model):
+    SK_ID_CURR = db.Column(db.Integer, primary_key=True)
+    SK_ID_BUREAU = db.Column(db.Integer, primary_key=True)
+    CREDIT_ACTIVE = db.Column(db.String(120))
+    CREDIT_CURRENCY = db.Column(db.String(120))
+    DAYS_CREDIT = db.Column(db.Integer)
+    CREDIT_DAY_OVERDUE = db.Column(db.Integer)
+    DAYS_CREDIT_ENDDATE = db.Column(db.String(120))
+    DAYS_ENDDATE_FACT = db.Column(db.String(120))
+    AMT_CREDIT_MAX_OVERDUE = db.Column(db.String(120))
+    CNT_CREDIT_PROLONG = db.Column(db.Integer)
+    AMT_CREDIT_SUM = db.Column(db.Float)
+    AMT_CREDIT_SUM_DEBT = db.Column(db.String(120))
+    AMT_CREDIT_SUM_LIMIT = db.Column(db.String(120))
+    AMT_CREDIT_SUM_OVERDUE = db.Column(db.Float)
+    CREDIT_TYPE = db.Column(db.String(120))
+    DAYS_CREDIT_UPDATE = db.Column(db.Integer)
+    AMT_ANNUITY = db.Column(db.String(120))
--- web_app_flask/app/routes.py
+++ web_app_flask/app/routes.py
@@ -1,10 +1,10 @@
 from flask import render_template, request, redirect
-from app import app
+from app import app, db
 from app.models import *
 
 
-@app.route('/', methods=['GET'], defaults={"page": 1})
-@app.route('/<int:page>', methods=['GET'])
+@app.route('/test', methods=['GET'], defaults={"page": 1})
+@app.route('/test<int:page>', methods=['GET'])
 def homepage(page):
     page = page
     per_page = 10
@@ -12,6 +12,26 @@ def homepage(page):
     return render_template('index.html', content=content)
 
 
-@app.route('/test', methods=['GET'])
-def test():
-    return render_template('test.html')
+@app.route('/', methods=['GET'])
+def dashboard():
+
+    top_client_nb_credit = db.engine.execute('SELECT SK_ID_CURR, count(*) AS nb_client '
+                               'FROM bureau '
+                               'GROUP BY SK_ID_CURR '
+                               'ORDER BY nb_client DESC '
+                               'LIMIT 5;')
+
+    # Formatage pour l'affichage avec graph.js
+    labels = "["
+    data = "["
+    for one in top_client_nb_credit:
+        labels += "#"+str(one.SK_ID_CURR)+","
+        data += str(one.nb_client)+","
+    labels = labels[:-1]+"]"
+    data = data[:-1] + "]"
+
+    return render_template(
+        'dashboard.html',
+        labels=labels,
+        data=data
+    )
