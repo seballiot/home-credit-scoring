28f3b3fc06b1c9c71e8f1eb1ab12eb3d3cf1bdd3
seb <sebastien.alliot@eemi.com>
2021-01-27 00:16:58 +0100

    Ajout page 1 client

--- web_app_flask/app/routes.py
+++ web_app_flask/app/routes.py
@@ -1,7 +1,6 @@
 from flask import render_template, request, redirect, url_for, Markup
 from app import app, db
 from app.models import *
-import html.entities
 
 
 @app.route('/test', methods=['GET'], defaults={"page": 1})
@@ -17,29 +16,30 @@ def homepage(page):
 def dashboard():
 
     top_client_nb_credit = db.engine.execute('SELECT SK_ID_CURR, count(*) AS nb_client '
-                               'FROM bureau '
-                               'GROUP BY SK_ID_CURR '
-                               'ORDER BY nb_client DESC '
-                               'LIMIT 10;')
-
-    nb_client_homme = db.engine.execute('SELECT COUNT(DISTINCT SK_ID_CURR) AS nb_homme '
-                                        'FROM application_train '
-                                        'WHERE CODE_GENDER = "M";')
-    nb_client_homme = nb_client_homme.fetchone()['nb_homme']
-
-    nb_client_femme = db.engine.execute('SELECT COUNT(DISTINCT SK_ID_CURR) AS nb_femme '
-                                        'FROM application_train '
-                                        'WHERE CODE_GENDER = "F";')
-    nb_client_femme = nb_client_femme.fetchone()['nb_femme']
-
-    # Formatage pour l'affichage du graph
+                                             'FROM bureau '
+                                             'GROUP BY SK_ID_CURR '
+                                             'ORDER BY nb_client DESC '
+                                             'LIMIT 10;')
+    # Formatage du resultat pour l'affichage du graph
     labels = "["
     data = "["
     for one in top_client_nb_credit:
-        labels += "#"+str(one.SK_ID_CURR)+","
-        data += str(one.nb_client)+","
-    labels = labels[:-1]+"]"
+        labels += "#" + str(one.SK_ID_CURR) + ","
+        data += str(one.nb_client) + ","
+    labels = labels[:-1] + "]"
     data = data[:-1] + "]"
+
+    client_gender = db.engine.execute('SELECT CODE_GENDER, COUNT(DISTINCT SK_ID_CURR) as total '
+                                      'FROM application_train '
+                                      'GROUP BY CODE_GENDER;')
+    # Formatage du resultat pour l'affichage du graph
+    for gender in client_gender:
+        if gender['CODE_GENDER'] == "M":
+            nb_client_homme = gender['total']
+        elif gender['CODE_GENDER'] == "F":
+            nb_client_femme = gender['total']
+        else:
+            nb_client_na = gender['total']
 
     return render_template(
         'dashboard.html',
@@ -47,6 +47,7 @@ def dashboard():
         data=data,
         nb_client_homme=nb_client_homme,
         nb_client_femme=nb_client_femme,
+        nb_client_na=nb_client_na,
         page='dashboard'
     )
 
@@ -70,11 +71,16 @@ def get_client(id):
     id = Markup.escape(id)
 
     client = ApplicationTrain.query.get(id)
+    moy_revenu = db.engine.execute('SELECT AVG(AMT_INCOME_TOTAL) as moy_revenu FROM application_train;')
+    moy_revenu = round(moy_revenu.fetchone()['moy_revenu'], 2)
+    perc_ecart = round((client.AMT_INCOME_TOTAL - moy_revenu) / moy_revenu * 100)
 
     return render_template(
         'client.html',
         client=client,
-        page='client'
+        page='client',
+        moy_revenu=moy_revenu,
+        perc_ecart=perc_ecart
     )
 
 
